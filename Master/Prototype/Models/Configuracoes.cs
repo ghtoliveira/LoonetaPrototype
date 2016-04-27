6a08f9c19962934e8569926c397f9885b2700d79
Guilherme Oliveira <guilhermehoaula@outlook.com>
2016-04-27 13:41:52 -0300

    First Commit

---
+++ Master/Prototype/Models/Configuracoes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prototype.Models {
+    public class Configuracoes {
+        public int UsuarioID { get; set; }
+        public int RaioDeReclamacoes { get; set; }
+    }
+}
